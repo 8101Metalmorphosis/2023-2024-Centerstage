52a5061f47def893b4e83dd18237216e9ca67059
MetalMorphosis8101 <ftc8101@gmail.com>
2024-01-13 12:49:14 -0600

    Update StateTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StateTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StateTeleOp.java
@@ -149,7 +149,7 @@ public class StateTeleOp extends LinearOpMode {
 
                             firstState = false;
                         }
-                        
+
 
                         // Move arm to intake position
                         if(robot.lifter.currentArmPosition == Constants.LifterConstants.liftArmIntake &&
